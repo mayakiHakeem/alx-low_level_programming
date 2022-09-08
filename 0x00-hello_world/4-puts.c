ef8451d2c932294189d20dc4152033fe4b2b22a3
mayakiHakeem <mayakijnr@gmail.com>
2022-09-08 08:32:02 -0700

    running-on-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main(void)
+int main(void) /* Uses puts() to display a string */
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
