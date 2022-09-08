8c668a917dc9e5d3eb39acece54e607d9966923b
mayakiHakeem <mayakijnr@gmail.com>
2022-09-08 09:01:50 -0700

    puts_as-betty-wants

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-/* Uses puts() to display a string */
-int main(void) 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
