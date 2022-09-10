cf9b89db66e044d5deb26f4dd74f5ac606a6c10c
mayakiHakeem <mayakijnr@gmail.com>
2022-09-10 16:39:05 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar(10);
+	return (0);
+}
