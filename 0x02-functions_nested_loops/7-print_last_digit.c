37181912eed140555d66420e67e8605aed585ee8
mayakiHakeem <mayakijnr@gmail.com>
2022-09-14 15:50:45 -0700

    Print last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ * Description: print last deigit of entered number
+ * @int: type of formal parameters
+ * Return: an integer
+ */
+
+int print_last_digit(int)
+{
+	int last = n % 10;
+
+	if (n < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
