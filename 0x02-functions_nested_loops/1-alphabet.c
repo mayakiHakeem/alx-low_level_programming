5739cb8b55fdc8c60c1b5cb304c8e5e093da37c5
mayakiHakeem <mayakijnr@gmail.com>
2022-09-13 17:04:35 -0700

    Add task to print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print the alphabet in lower case to stdout
+ * 
+ * Return: On success 1.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return;
+}	
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define _MAIN_HEADER
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
