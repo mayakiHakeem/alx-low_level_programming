42664494aa92c46fe31ee3c5cf87f4138b73140c
mayakiHakeem <mayakijnr@gmail.com>
2022-09-14 01:36:55 -0700

    add

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _isalpha - Entry Point
+ * Description - check if argument is an alphabet
+ *@c: formal parameter of the function
+ * Return: 1 for True and 0 for False
+ */
+int _isalpha(int c)
+{
+	int i;
+
+	i = 'a';
+	while (( 90 >= i >= 65) || (122 >= i >= 97))
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);	
+}
