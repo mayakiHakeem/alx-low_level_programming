933fc730e16d2b8374370a7dd0d629c356df1f12
mayakiHakeem <mayakijnr@gmail.com>
2022-09-14 14:57:00 -0700

    add_print-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ * Description: print entry point of sign
+ * @n: formal parameter of the argument
+ * Return: 0 if n == 0, 1 if n == positive and -1 if n == negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(53);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar(55);
+		return (-1);
+	}
+}
