9fd20faa77d0c01fe2b54e5b6cd07dee917d2527
mayakiHakeem <mayakijnr@gmail.com>
2022-09-13 17:26:19 -0700

    Print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10x in 10 lines
+ *
+ * Return: always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	i = 1;
+	while (i <= 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
