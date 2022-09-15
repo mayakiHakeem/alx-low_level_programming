2667e73dfb9ddedbd479c4ae30e440b453d62cf3
mayakiHakeem <mayakijnr@gmail.com>
2022-09-14 18:21:17 -0700

    Print natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print natural numbers
+ * description: print all natural numbers from given number to 98
+ * @n: given parameter
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		if (i != '98')
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
