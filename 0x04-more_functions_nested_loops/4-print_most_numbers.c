30fe7b764016ba26b6ed7432400d67714a39def3
mayakiHakeem <mayakijnr@gmail.com>
2022-09-17 09:44:15 -0700

    Print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_most_numbers - print number set
+ *
+ * Return: integral result
+ */
+
+print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (!(i == 2 || i == 4))
+		{
+			_putchar(i + '0');
+		}
+	}
+		putchar('\n');
+}
