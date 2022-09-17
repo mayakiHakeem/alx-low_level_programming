1c833e24d4a987b9c22c6001b5be61909f99ab79
mayakiHakeem <mayakijnr@gmail.com>
2022-09-17 11:16:18 -0700

    print diagonal lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_diagonal - print diagonal lines
+ * @n: integral formal parameter
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
