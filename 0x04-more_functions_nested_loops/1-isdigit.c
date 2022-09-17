3821c366338e3ec438d3e65f38dfd265064e7026
mayakiHakeem <mayakijnr@gmail.com>
2022-09-17 09:01:56 -0700

    check if digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if argument is digit
+ * @c: integral formal parameter of the function
+ * Return: if true 1 if false 0
+ */
+int _is digit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
