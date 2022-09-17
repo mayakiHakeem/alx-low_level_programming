98a9f441145572ea493fdd801d9d742ad0acaca6
mayakiHakeem <mayakijnr@gmail.com>
2022-09-17 09:16:59 -0700

    Multiply 2 integral arguments

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * mul - multiplied 2 integral arguments
+ * @a: integral formal parameter
+ * @b: second integral formal parameter
+ * Return: integral result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
