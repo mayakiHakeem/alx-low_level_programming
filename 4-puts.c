852bea22ebc2fcfd50681aec19221cc2f67c9cf3
mayakihakeem <mayakijnr@gmail.com>
2023-05-18 18:33:14 -0700

    puts it out

---
+++ 4-puts.c
@@ -0,0 +1,13 @@
+/**
+ * puts
+ *
+ * Description: Printing string
+ * with the puts function.
+ */
+
+#include <stdio.h>
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle");
+  return 0;
+}
