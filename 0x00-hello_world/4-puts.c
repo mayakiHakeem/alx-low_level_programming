1c836d8d537c131fac9cdc7263e4d01fd216f728
mayakihakeem <mayakijnr@gmail.com>
2023-05-18 20:24:32 -0700

    puts it like a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: put string
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
