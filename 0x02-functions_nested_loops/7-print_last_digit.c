86e35fd09c218cb452c71ea13a8ba60d9d7a80bd
mayakihakeem <mayakijnr@gmail.com>
2023-05-24 07:44:36 -0700

    Write the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <unistd.h>
 
 /**
@@ -12,5 +13,6 @@ int print_last_digit(int n)
 	int last_digit;
 
 	last_digit = n % 10;
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
