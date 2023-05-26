4f98471d762423ae82670dd2cb0880845d1c8b71
mayakihakeem <mayakijnr@gmail.com>
2023-05-25 18:40:42 -0700

    Check numbers

--- 0x04-more_functions_nested_loops/test.c
+++ 0x04-more_functions_nested_loops/test.c
@@ -8,9 +8,11 @@
  */
 int main(void)
 {
-	print_line(0);
-	print_line(2);
-	print_line(10);
-	print_line(-4);
+	char c;
+
+	c = '0';
+	printf("%c: %d\n", c, _isdigit(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isdigit(c));
 	return (0);
 }
