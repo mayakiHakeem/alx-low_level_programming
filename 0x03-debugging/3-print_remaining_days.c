7a669e9ac320947907ccc308a4356f77db64d698
mayakihakeem <mayakijnr@gmail.com>
2023-05-27 18:01:01 -0700

    Commit the date check

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,11 +12,13 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0  && year % 100 != 0) || (year % 400 == 0))
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		printf("wew");
+		if (month > 2)
 		{
-			/* day++; */
+			printf("%d\n", day);
+			day++;
 		}
 
 		printf("Day of the year: %d\n", day);
