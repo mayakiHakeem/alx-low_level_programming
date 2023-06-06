172057629eab928a62985837f716a6d41baeaf5b
mayakihakeem <mayakijnr@gmail.com>
2023-06-06 03:01:32 -0700

    Solved 0-strcat function removing an unused variable

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,6 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int len = 0;
 	char *dest_ptr = dest;
 
 	while (*dest != '\0')
--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -9,11 +9,15 @@
  */
 int main(void)
 {
-	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
 	char *ptr;
 
-	ptr = cap_string(str);
+	printf("%s\n", s1);
+	printf("%s", s2);
+	ptr = _strcat(s1, s2);
+	printf("%s", s1);
+	printf("%s", s2);
 	printf("%s", ptr);
-	printf("%s", str);
 	return (0);
 }
