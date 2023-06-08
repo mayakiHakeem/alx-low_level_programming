dbe973ee0b9bc42ddebdba919000fa34aa3702a7
mayakihakeem <mayakijnr@gmail.com>
2023-06-08 15:22:53 -0700

    Add a function that print a chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,12 +13,8 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0;  j < 8; j++)
 		{
-			if (i == 0 || i == 7)
-			{
-				_putchar(a[i][j]);
-				if (j == 7)
-					_putchar('\n');
-			}
+			_putchar(a[i][j]);
 		}
+		_putchar('\n');
 	}
 }
