31eb3c0f1b666f5d7d7fb2019051f31b35947916
mayakihakeem <mayakijnr@gmail.com>
2023-06-08 11:36:17 -0700

    Refine the custom strstr function implementation to return NULL correctly

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,21 +12,20 @@ char *_strstr(char *haystack, char *needle)
 {
 	if (*needle == '\0')
 		return (haystack);
-	for (; *needle != '\0'; haystack++)
-	{
-		if (*needle == *haystack)
-		{
-			char *need2 = needle;
-			char *hay2 = haystack;
 
-			while (*need2 != '\0' && *need2 == *hay2)
-			{
-				hay2++;
-				need2++;
-			}
-			if (*need2 == '\0')
-				return (haystack);
+	while (*haystack != '\0')
+	{
+		char *need2 = needle;
+		char *hay2 = haystack;
+
+		while (*need2 != '\0' && *need2 == *hay2)
+		{
+			hay2++;
+			need2++;
 		}
+		if (*need2 == '\0')
+			return (haystack);
+		haystack++;
 	}
 	return (NULL);
 }
