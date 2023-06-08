7ad948c57aa992e3b8be655029a7a8954addbfb2
mayakihakeem <mayakijnr@gmail.com>
2023-06-08 15:25:59 -0700

    Fix betty style and betty document errors for the print_chessboard function

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * 7-print_chessboard - print chesboard
+ * print_chessboard - print chesboard
  * @a: array to be printed
  *
  * Return: void
@@ -9,6 +9,7 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0;  j < 8; j++)
