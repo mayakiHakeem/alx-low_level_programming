240ef2a7e53b0934daf9380ffaef6e3bb60d28a1
mayakihakeem <mayakijnr@gmail.com>
2023-06-14 15:13:51 -0700

    Wrote functions that dynamically allocate memory and initialize it

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,11 +17,8 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	array = malloc(sizeof(char) * size);
 	if (array == NULL)
-	       return (NULL);
-	else
-	{
-		for (i = 0; i <= size; i++)
-		       array[i] = c;
-	}
-		       return (array);
+		return (NULL);
+	for (i = 0; i <= size; i++)
+		array[i] = c;
+	return (array);
 }
