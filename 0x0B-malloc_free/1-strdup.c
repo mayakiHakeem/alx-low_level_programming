65d01d2686ef6f09edd58caf3863c689a066f143
mayakihakeem <mayakijnr@gmail.com>
2023-06-14 15:56:01 -0700

    Commit the solution to 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup -  returns ptr a space in memory of a copy of str parameter
+ * @str: copied sring
+ *
+ * Return: 1 if size is 0, ptr to str if > 0. NULL if it fails
+ */
+char *_strdup(char *str)
+{
+	char *new_str, *ptr_newstr;
+	int len = 0;
+
+	while (*str != '\0')
+		len++;
+	new_str = malloc(sizeof(char) * len);
+	ptr_newstr = new_str;
+	if (new_str == NULL)
+		return (NULL);
+	while (*str != '\0')
+	{
+		*new_str = *str;
+		new_str++;
+		str++;
+	}
+	return (ptr_newstr);
+}
