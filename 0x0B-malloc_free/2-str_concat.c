445a439437492fdc3388068da6e6fffdc9cea2cc
Mayaki Hakeem <mayakijnr@gmail.com>
2023-08-08 17:34:23 -0700

    Correct error i the field

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,11 @@ char *str_concat(char *s1, char *s2)
 	char *concat;
 	int i, j, k, len = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (i = 0; s1[i] != '\0'; i++)
 		len++;
 	for (j = 0; s2[j] != '\0'; j++)
