222763630e46b54d835b518c301cca4b3187a6da
Mayaki Hakeem <mayakijnr@gmail.com>
2023-08-10 16:03:31 -0700

    Add string_nconcat function for task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenate strings
+ * @s1: string to concatenate to
+ * @s2: string to concatenate with
+ * @n: size of @s2 to concatenate @s1 with
+ * Description: concatenate first @n bytes of @s2 to @s1
+ *
+ * Return: ptr to concatenated strings
+ * NULL if failed
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len_s1, len_s2, i, j;
+	char *ptr_str;
+
+	if (s1[0] == '\0')
+		s1 = "";
+	if (s2[0] == '\0')
+		s2 = "";
+
+	for (len_s1 = 0; s1[len_s1]; len_s1++)
+	{
+	}
+	for (len_s2 = 0; s2[len_s2]; len_s2++)
+	{
+	}
+
+	if (n >= len_s2)
+		n = len_s2;
+
+	ptr_str = malloc(len_s1 + n + 1);
+	if (ptr_str == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		ptr_str[i] = s1[i];
+
+	for (j = 0; j < n; j++, i++)
+		ptr_str[i] = s2[j];
+	ptr_str[i] = '\0';
+
+	return (ptr_str);
+}
