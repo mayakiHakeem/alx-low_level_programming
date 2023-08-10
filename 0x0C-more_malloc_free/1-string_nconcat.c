9951e73766ffe63cb8a870e27ecf4a0a83da6a9f
Mayaki Hakeem <mayakijnr@gmail.com>
2023-08-10 16:10:24 -0700

    Add string_nconcat function with corrected memory allocation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len_s2)
 		n = len_s2;
 
-	ptr_str = malloc(len_s1 + n + 1);
+	ptr_str = malloc(sizeof(char) * (len_s1 + n + 1));
 	if (ptr_str == NULL)
 		return (NULL);
 
