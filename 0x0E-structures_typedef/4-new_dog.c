f4ce3568850829385155ad1bbc9d8d39b78c9ab9
mayakiHakeem <mayakijnr@.com>
2023-08-15 00:58:25 +0100

    Handle copying of name and owner members in task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "dog.h"
 #include <stdlib.h>
 
@@ -17,9 +18,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog1 = malloc(sizeof(dog_t));
 	if (dog1 == NULL)
 		return (NULL);
-	dog1->name = name;
-	dog1->age = age;
-	dog1->owner = owner;
 
+	dog1->name = strdup(name);
+	if (dog1->name == NULL)
+	{
+		free(dog1);
+		return (NULL);
+	}
+
+	dog1->age = age;
+
+	dog1->owner = strdup(owner);
+	if (dog1->owner == NULL)
+	{
+		free(dog1->name);
+		free(dog1);
+		return (NULL);
+	}
 	return (dog1);
 }
