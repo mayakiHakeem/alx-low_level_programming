cfbd729cc367d7756a6b5c0905329c3eeed114cd
mayakiHakeem <mayakijnr@.com>
2023-08-15 02:37:47 +0100

    Resolve NULL condition of d

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,16 +10,16 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d)
 	{
-		if (d->name != NULL)
+		if (d->name)
 			printf("Name: %s\n", d->name);
 		else
 			printf("(nil)");
 
 		printf("Age: %f\n", d->age);
 
-		if (d->owner != NULL)
+		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
 			printf("(nil)");
