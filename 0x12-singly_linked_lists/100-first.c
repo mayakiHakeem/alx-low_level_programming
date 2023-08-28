30dc011aec2a87d792bca13c69099619f6704347
mayakiHakeem <mayakijnr@.com>
2023-08-28 18:17:38 +0100

    Resolve betty errors for advanced tasks 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void __attribute__((constructor)) first_msg(void);
 /**
- * print_msg - prints message before main routine executed
+ * first_msg - prints message before main routine executed
  *
  * Return: void
  */
