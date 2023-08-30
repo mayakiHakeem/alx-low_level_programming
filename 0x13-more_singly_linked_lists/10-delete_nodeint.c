a552ed774cfc51287100d02f1587fa5aee102950
mayakiHakeem <mayakijnr@.com>
2023-08-31 00:37:03 +0100

    Add function to delete node at index for task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,4 +10,29 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int i;
+	listint_t *prev, *temp;
+
+	if (!head || !*head) /* Check if head or list is NULL*/
+		return (-1);
+
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+
+	prev = *head;
+	for (i = 0; prev != NULL && i < index - 1; i++)
+		prev = prev->next;
+
+	if (prev == NULL || prev->next == NULL)
+		return (-1);
+
+	temp = prev->next;
+	prev->next = temp->next;
+	free(temp);
+	return (1);
 }
