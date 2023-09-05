a0f4d4f25363830ffe7a762001869439117539ee
mayakiHakeem <mayakijnr@.com>
2023-09-05 03:07:37 +0100

    Add task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,8 +22,12 @@ int create_file(const char *filename, char *text_content)
 
 	writer = write(fd, text_content, _strlen(text_content));
 	if (writer == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 
+	close(fd);
 	return (1);
 }
 
@@ -35,7 +39,7 @@ int create_file(const char *filename, char *text_content)
  */
 int _strlen(char *str)
 {
-	int len, i;
+	int i, len = 0;
 
 	i = 0;
 	while (str[i])
