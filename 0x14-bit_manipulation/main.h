ff93849dda8f66f86f9f0c33619d77dc62ffeca0
mayakiHakeem <mayakijnr@.com>
2023-09-13 14:18:33 +0100

    Add task 2

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,6 @@ void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int _strlen(const char *str);
 int pow2(int);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
