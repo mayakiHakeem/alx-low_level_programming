2b557f758a60644f22fe11d52a2004b84e168116
mayakiHakeem <mayakijnr@gmail.com>
2024-02-05 14:44:12 +0100

    Resolve args to string function

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -18,7 +18,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
-		total_len += _strlen(av[i]);
+		total_len += (_strlen(av[i]) + 1);
 
 	ptr_str = malloc(sizeof(char) * total_len + 1);
 	if (!ptr_str)
