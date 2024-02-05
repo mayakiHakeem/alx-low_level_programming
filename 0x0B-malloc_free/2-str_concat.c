f6cc2b067f366e13afebd475071d8002cc7946f4
mayakiHakeem <mayakijnr@gmail.com>
2024-02-05 14:30:01 +0100

    Resolve concatenate function to handle NULL string as empty strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,6 +25,8 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; s2[j]; j++)
 		len++;
 	conc_str = malloc(len * sizeof(char) + 1);
+	if (!conc_str)
+		return (NULL);
 
 	for (i = 0; s1[i]; i++)
 		conc_str[i] = s1[i];
