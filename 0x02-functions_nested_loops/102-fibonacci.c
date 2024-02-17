03112bdd9e6c86cedef3579ed97b0fb3e632e543
mayakiHakeem <mayakijnr@gmail.com>
2024-02-17 12:17:13 +0100

    Add files to solve fibonaci sequence tasks 102 and 103

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned long int prev1 = 1, prev2 = 2, fib;
+	int i;
+
+	printf("%ld, %ld, ", prev1, prev2);
+
+	for (i = 1; i <= 48; i++)
+	{
+		fib = prev1 + prev2;
+		printf("%ld", fib);
+		prev1 = prev2;
+		prev2 = fib;
+
+		if (i != 48)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	long int num1 = 1, num2 = 2, fib, total = 2;
+	int i;
+
+	for (i = 0; i <= 48; i++)
+	{
+		fib = num1 + num2;
+		if (fib > 4000000)
+			break;
+		num1 = num2;
+		num2 = fib;
+
+		if (fib % 2 == 0)
+			total += fib;
+	}
+
+	printf("%ld\n", total);
+	return (0);
+}
