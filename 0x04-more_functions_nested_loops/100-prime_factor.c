9d568fccb19db9e751439390e4f58f4f99e1c7a2
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-19 15:05:03 +0100

    Fixed code for printing largest prime number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,47 +1,57 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int isPrime(long int number);
-
+/**
+ * main - print highest prime number
+ *
+ * Return: 0 (success)
+ */
 int main(void)
 {
-  long int num = 612852475143;
-  long int i, largestPrime = 2;
+	long int num;
+	long int i, largestPrime = 2;
 
-  while (num % 2 == 0)
-  {
-    largestPrime = 2;
-    num /= 2;
-  }
+	while (num % 2 == 0)
+	{
+		largestPrime = 2;
+		num /= 2;
+	}
 
-  for (i = 3; i * i <= num; i += 2)
-  {
-    if (num % i == 0 && isPrime(i))
-    {
-      largestPrime = i;
-      num /= i;
-    }
-  }
+	for (i = 3; i * i <= num; i += 2)
+	{
+		if (num % i == 0 && isPrime(i))
+		{
+			largestPrime = i;
+			num /= i;
+		}
+	}
 
-  if (num > 2 && isPrime(num))
-    largestPrime = num;
+	if (num > 2 && isPrime(num))
+		largestPrime = num;
 
-  printf("Largest Prime Factor: %ld\n", largestPrime);
+	printf("Largest Prime Factor: %ld\n", largestPrime);
 
-  return 0;
+	return (0);
 }
 
+/**
+ * isPrime - check if an int is prime
+ * @number: number to check
+ *
+ * Return: 0 (false) | 1 (true)
+ */
 int isPrime(long int number)
 {
-  long int i;
-  if (number <= 1)
-    return (0);
+	long int i;
 
-  for (i = 2; i * i <= number; i++)
-  {
-    if (number % i == 0)
-      return (0);
-  }
+	if (number <= 1)
+		return (0);
 
-  return (1);
+	for (i = 2; i * i <= number; i++)
+	{
+		if (number % i == 0)
+			return (0);
+	}
+
+	return (1);
 }
