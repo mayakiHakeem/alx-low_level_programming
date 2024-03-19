92734ba586495a169dd8f0bea6ad42124fd9f9ba
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-19 16:03:46 +0100

    Fix functon to print lon number with putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+void print_number(int n)
+{
+	int count = 0, temp, i, places = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -(n);
+	}
+
+	temp = n;
+
+	while (temp != 0)
+	{
+		temp /= 10;
+		count++;
+	}
+
+	for (i = 1; i < count; i++)
+		places *= 10;
+	while (places > 0)
+	{
+		_putchar((n / places) + '0');
+		n %= places;
+		places /= 10;
+	}
+}
