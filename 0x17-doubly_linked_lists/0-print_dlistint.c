f769a35612170525dd3e4442a3372691f6a2f2f3
mayakiHakeem <mayakijnr@gmail.com>
2024-03-22 21:53:52 +0100

    added task 0 of doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
 
@@ -10,7 +9,7 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    	int count = 0;
+    	size_t count = 0;
 	
 	if (!h)
 		return (count);
