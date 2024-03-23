4e244f38d423200fd928bc56044c87b38cc4b45b
mayakiHakeem <mayakijnr@gmail.com>
2024-03-23 01:42:18 +0100

    highest palindromes

--- 0x17-doubly_linked_lists/tets.py
+++ 0x17-doubly_linked_lists/tets.py
@@ -2,9 +2,12 @@
 
 def is_palindrome(text):
     return text == text[::-1]
-
+high = 0
 for i in range(100, 1000):
     for j in range(100, 1000):
         res = str(i * j);
         if is_palindrome(res):
-            print(res)
+            res = int(res)
+            if res > high:
+                high = res
+print(high)
