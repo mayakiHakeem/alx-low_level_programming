d2d6acb920c5681dc3519ce3fa0f0d6edffaa68f
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-23 15:10:19 +0100

    Add node at the end of doubly lists

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -22,7 +22,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = new; /*If the list is empty, new becomes head*/
-		return new;
+		return (new);
 	}
 	temp = *head;
 	while (temp->next)
