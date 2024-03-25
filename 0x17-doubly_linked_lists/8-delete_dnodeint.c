2753738c0814bbd606be9cc43c7c664400fc59b8
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-25 10:31:46 +0100

    Fixed accessing NULL pointers when deleting at the end

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,32 +13,29 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i, count = node_count(head);
 	dlistint_t *temp = *head;
 
+	if (index >= count)
+		return (-1);
 	if (index == 0)
 	{
 		*head = (*head)->next;
+		if (*head)
+			(*head)->prev = NULL;
 		free(temp);
-		temp = NULL;
-		(*head)->prev = NULL;
-		return (1);
-	}
-	if (index >= count)
-		return (-1);
-	if (index == count - 1)
-	{
-		while (temp->next)
-			temp = temp->next;
-		free(temp);
-		temp = NULL;
 		return (1);
 	}
 	for (i = 0; i < index; i++)
 		temp = temp->next;
+
+	if (!temp)
+		return (-1);
+
 	temp->prev->next = temp->next;
-	temp->next = temp->next->prev;
+	if (temp->next)
+		temp->next->prev = temp->prev;
 	free(temp);
-	temp = NULL;
 	return (1);
 }
+
 /**
  * node_count - count number of nodes
  * @head: pointer to address of the head of list
