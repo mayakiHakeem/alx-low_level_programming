7fc76fad8e4a0ed17fbe55e78a789ab74c5eb453
mayaki <mayakijnr@gmail.com>
2024-07-19 10:13:18 +0100

    Solve task 4 of hash table project

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,9 +12,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *temp;
 
+	if (!ht || !key || !*key)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
-	while(temp)
+	while (temp)
 	{
 		if (strcmp(temp->key, key) == 0)
 			return (temp->value);
