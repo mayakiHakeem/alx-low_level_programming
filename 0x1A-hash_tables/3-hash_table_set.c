4b5f54b27d70684367aeb4e7db3ba71e048b796c
mayaki <mayakijnr@gmail.com>
2024-07-19 09:32:57 +0100

    Resolve checker errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,33 +12,42 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *current, *new_node;
 	unsigned long int index;
+	char *value_temp;
 
-	if (!key || *key)
+	if (!ht || !key || !*key || !value)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
-
 	current = ht->array[index];
+	value_temp = strdup(value);
+	if (!value_temp)
+		return (0);
 
-	while (!current)
+	while (current)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
 			free(current->value);
-			current->value = strdup(value);
+			current->value = value_temp;
 			return (1);
 		}
 		current = current->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
-
 	if (!new_node)
+	{
+		free(value_temp);
 		return (0);
+	}
 
 	new_node->key = strdup(key);
-	new_node->value = strdup(value);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = value_temp;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
