b57f8b448b70fe6d08d599c875e503d590de49d8
mayaki <mayakijnr@gmail.com>
2024-07-19 10:08:02 +0100

    Solve task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieve values of supplied keys
+ * @ht: hash table to search for
+ * @key: the key to retrieve value for in table @ht
+ *
+ * Return: value associated with key (on success) | NULL (failure)
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while(temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+
+	return (NULL);
+}
