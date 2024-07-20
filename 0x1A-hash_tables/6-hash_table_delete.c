e7f1f5a42af17b77b4044d9b0694e503a780b2e2
mayaki <mayakijnr@gmail.com>
2024-07-20 10:44:39 +0100

    Solve task 6 of hash tables project

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete a hash table
+ * @ht: ptr to hash table to delete
+ *
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node, *temp;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while(node)
+		{
+			free(node->key);
+			free(node->value);
+			temp = node;
+			node = node->next;
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
