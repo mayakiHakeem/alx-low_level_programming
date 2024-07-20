82fe4e5b596621f529e380923aac61572d87281e
mayaki <mayakijnr@gmail.com>
2024-07-20 09:55:12 +0100

    Fix comma separation of k-v pairs

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -24,7 +24,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (!flag)
 				printf(", ");
-			printf("'%s':'%s'", node->key, node->value);
+			printf("'%s': '%s'", node->key, node->value);
 			flag = 0;
 			node = node->next;
 		}
